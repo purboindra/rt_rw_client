5e8421fe25a730b231782c9b6d040f20a0947a94
purboyndra <purboindra@gmail.com>
2025-07-02 06:50:21 +0700

    feat: Implement signIn in AuthViewModel.kt

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/auth/presentation/AuthViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/auth/presentation/AuthViewModel.kt
@@ -1,10 +1,13 @@
 package org.purboyndradev.rt_rw.features.auth.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import org.purboyndradev.rt_rw.core.domain.Result
+import org.purboyndradev.rt_rw.domain.usecases.SignInUseCase
 
 data class OTPUiState(
     val otpLength: Int = 6,
@@ -12,13 +15,16 @@ data class OTPUiState(
     val otpValues: List<String> = List(otpLength) { "" },
 )
 
-class AuthViewModel() : ViewModel() {
+class AuthViewModel(
+    private val signInUseCase: SignInUseCase
+) : ViewModel() {
     
     private val _otpUiState: MutableStateFlow<OTPUiState> =
         MutableStateFlow(OTPUiState())
     val otpUiState = _otpUiState.asStateFlow()
     
-    private val _whatsAppNumberState: MutableStateFlow<String> = MutableStateFlow("")
+    private val _whatsAppNumberState: MutableStateFlow<String> =
+        MutableStateFlow("")
     val whatsAppNumberState = _whatsAppNumberState.asStateFlow()
     
     fun updateOtpValue(index: Int, value: String) {
@@ -32,9 +38,23 @@ class AuthViewModel() : ViewModel() {
         }
     }
     
-    fun onUpdateWhatsAppNumber(whatsAppNumber: String){
+    fun onUpdateWhatsAppNumber(whatsAppNumber: String) {
         _whatsAppNumberState.update {
             whatsAppNumber
+        }
+    }
+    
+    fun signIn() {
+        viewModelScope.launch {
+            val result = signInUseCase.execute(_whatsAppNumberState.value)
+            when (result) {
+                is Result.Success -> {
+                    println("Success: ${result.data}")
+                }
+                is Result.Error -> {
+                    println("Error: ${result.error.name}")
+                }
+            }
         }
     }
     
