8cf1c51cc20f797904b5c60a5ff5acea8e437278
purboyndra <purboindra@gmail.com>
2025-07-02 06:29:46 +0700

    feat: Implement AuthRepositoryImpl.kt

---
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/repository/AuthRepositoryImpl.kt
@@ -0,0 +1,15 @@
+package org.purboyndradev.rt_rw.core.data.repository
+
+import org.purboyndradev.rt_rw.core.data.remote.api.AuthApi
+import org.purboyndradev.rt_rw.core.domain.DataError
+import org.purboyndradev.rt_rw.core.domain.Result
+import org.purboyndradev.rt_rw.domain.repository.AuthRepository
+
+class AuthRepositoryImpl(
+    private val api: AuthApi
+) : AuthRepository {
+    override suspend fun signIn(phoneNumber: String): Result<Boolean, DataError.Remote> {
+        val response = api.signIn(phoneNumber)
+        return response
+    }
+}
