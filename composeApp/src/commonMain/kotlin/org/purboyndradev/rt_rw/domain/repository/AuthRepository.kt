51646b9ddb108de21c5fd936147e1e5dbb35b9d1
purboyndra <purboindra@gmail.com>
2025-07-02 06:49:37 +0700

    feat: Implement AuthRepository.kt

---
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/repository/AuthRepository.kt
@@ -0,0 +1,8 @@
+package org.purboyndradev.rt_rw.domain.repository
+
+import org.purboyndradev.rt_rw.core.domain.DataError
+import org.purboyndradev.rt_rw.core.domain.Result
+
+interface AuthRepository {
+    suspend fun signIn(phoneNumber: String): Result<Boolean, DataError.Remote>
+}
