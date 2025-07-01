50a839222557d433edef677d3b0ed6678903ea71
purboyndra <purboindra@gmail.com>
2025-07-02 06:50:39 +0700

    feat: DI for auth

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/di/Model.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/di/Model.kt
@@ -6,7 +6,12 @@ import org.koin.core.module.Module
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.module
+import org.purboyndradev.rt_rw.core.data.remote.api.AuthApi
+import org.purboyndradev.rt_rw.core.data.remote.impl.KtorAuthRemoteDatasource
+import org.purboyndradev.rt_rw.core.data.repository.AuthRepositoryImpl
 import org.purboyndradev.rt_rw.core.network.HttpClientFactory
+import org.purboyndradev.rt_rw.domain.repository.AuthRepository
+import org.purboyndradev.rt_rw.domain.usecases.SignInUseCase
 import org.purboyndradev.rt_rw.features.auth.presentation.AuthViewModel
 
 fun initKoin(config: KoinAppDeclaration? = null) = startKoin {
@@ -24,6 +29,21 @@ val sharedModule: Module = module {
         HttpClientFactory.create(get())
     }
     
-    viewModel { AuthViewModel() }
+    /// PROVIDE REMOTE DATA SOURCE
+    single<AuthApi> {
+        KtorAuthRemoteDatasource(get())
+    }
     
+    /// PROVIDE REPOSITORY
+    single<AuthRepository> {
+        AuthRepositoryImpl(get())
+    }
+    
+    /// PROVIDE USE CASE
+    single {
+        SignInUseCase(get())
+    }
+    
+    /// PROVIDE VIEW MODEL
+    viewModel { AuthViewModel(get()) }
 }
