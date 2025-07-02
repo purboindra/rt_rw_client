ae07fe8c620431453cef214c8ce8fa0fdcba5f26
purboyndra <purboindra@gmail.com>
2025-07-03 05:35:07 +0700

    fix: Update signIn endpoint to use BASE_URL and set request body

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/remote/impl/KtorAuthRemoteDatasource.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/remote/impl/KtorAuthRemoteDatasource.kt
@@ -2,19 +2,19 @@ package org.purboyndradev.rt_rw.core.data.remote.impl
 
 import io.ktor.client.HttpClient
 import io.ktor.client.request.post
+import io.ktor.client.request.setBody
 import org.purboyndradev.rt_rw.core.data.remote.api.AuthApi
 import org.purboyndradev.rt_rw.core.domain.DataError
 import org.purboyndradev.rt_rw.core.domain.Result
 import org.purboyndradev.rt_rw.core.network.safeCall
+import org.purboyndradev.rt_rw.helper.BASE_URL
 
 class KtorAuthRemoteDatasource(private val httpClient: HttpClient) :
     AuthApi {
     override suspend fun signIn(phoneNumber: String): Result<Boolean, DataError.Remote> {
         return safeCall {
-            httpClient.post("http://localhost:3000/auth/sign-in") {
-                url {
-                    parameters.append("phone", phoneNumber)
-                }
+            httpClient.post("$BASE_URL/auth/sign-in") {
+                setBody(mapOf("phone" to phoneNumber))
             }
         }
     }
