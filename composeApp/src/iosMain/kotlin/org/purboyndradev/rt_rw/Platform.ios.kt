f8323a6022a7d18b201f90ea37a9c4dc0445e739
purboyndra <purboindra@gmail.com>
2025-07-03 05:36:19 +0700

    feat: Implement TelegramLauncher for iOS

--- composeApp/src/iosMain/kotlin/org/purboyndradev/rt_rw/Platform.ios.kt
+++ composeApp/src/iosMain/kotlin/org/purboyndradev/rt_rw/Platform.ios.kt
@@ -1,9 +1,24 @@
 package org.purboyndradev.rt_rw
 
 import platform.UIKit.UIDevice
+import platform.Foundation.NSURL
+import platform.UIKit.UIApplication
 
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
+class IOSPlatform : Platform {
+    override val name: String =
+        UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
 }
 
 actual fun getPlatform(): Platform = IOSPlatform()
+
+actual object TelegramLauncher {
+    actual fun open(url: String) {
+        val nsUrl = NSURL.URLWithString(url)
+        
+        if (nsUrl != null && UIApplication.sharedApplication.canOpenURL(nsUrl)) {
+            UIApplication.sharedApplication.openURL(nsUrl)
+        } else {
+            println("Error: Could not open URL: $url. It might be invalid or the required app (e.g., Telegram) is not installed.")
+        }
+    }
+}
