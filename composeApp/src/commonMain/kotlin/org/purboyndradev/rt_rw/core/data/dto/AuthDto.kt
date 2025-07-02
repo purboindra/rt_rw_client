9bdf190adf8864909b3c76744bff40666207dda3
purboyndra <purboindra@gmail.com>
2025-07-03 06:53:54 +0700

    feat: AuthDto.kt

---
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/dto/AuthDto.kt
@@ -0,0 +1,15 @@
+package org.purboyndradev.rt_rw.core.data.dto
+
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerialName
+
+@Serializable
+data class AuthDto(
+    /// This field for handle if got code USER_NOT_VERIFIED
+    @SerialName("redirect_url")
+    val redirectUrl: String? = null,
+    @SerialName("access_token")
+    val accessToken: String = "",
+    @SerialName("refresh_token")
+    val refreshToken: String = "",
+)
