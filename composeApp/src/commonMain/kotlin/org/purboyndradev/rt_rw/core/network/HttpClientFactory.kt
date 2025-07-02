19ae70473e2c27e9991e03577726032f6bb9f48b
purboyndra <purboindra@gmail.com>
2025-07-02 21:31:25 +0700

    feat: Configure HttpClient with logging and content negotiation

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/network/HttpClientFactory.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/network/HttpClientFactory.kt
@@ -6,6 +6,10 @@ import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.cache.HttpCache
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
@@ -17,11 +21,17 @@ object HttpClientFactory {
             install(ContentNegotiation) {
                 json(json = Json {
                     ignoreUnknownKeys = true
+                    prettyPrint = true
+                    isLenient = true
                 })
             }
             install(HttpTimeout) {
                 socketTimeoutMillis = 20_000L
                 requestTimeoutMillis = 20_000L
+            }
+            install(Logging) {
+                logger = Logger.DEFAULT
+                level = LogLevel.ALL
             }
             install(HttpCache)
             defaultRequest {
