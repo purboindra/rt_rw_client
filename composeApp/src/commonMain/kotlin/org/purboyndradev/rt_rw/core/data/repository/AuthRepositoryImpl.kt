67f0030f2d2f3b4688a201e51171f7341a04e75d
purboyndra <purboindra@gmail.com>
2025-07-04 06:56:15 +0700

    feat: Implement AuthRepositoryImpl.verifyOtp

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/repository/AuthRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/repository/AuthRepositoryImpl.kt
@@ -2,6 +2,7 @@ package org.purboyndradev.rt_rw.core.data.repository
 
 import org.purboyndradev.rt_rw.core.data.dto.SignInDto
 import org.purboyndradev.rt_rw.core.data.dto.ResponseDto
+import org.purboyndradev.rt_rw.core.data.dto.VerifyOtpDto
 import org.purboyndradev.rt_rw.core.data.remote.api.AuthApi
 import org.purboyndradev.rt_rw.core.domain.DataError
 import org.purboyndradev.rt_rw.core.domain.Result
@@ -12,6 +13,14 @@ class AuthRepositoryImpl(
 ) : AuthRepository {
     override suspend fun signIn(phoneNumber: String): Result<ResponseDto<SignInDto>, DataError.Remote> {
         val response = api.signIn(phoneNumber)
+        return response
+    }
+    
+    override suspend fun verifyOtp(
+        phoneNumber: String,
+        otp: String
+    ): Result<ResponseDto<VerifyOtpDto>, DataError.Remote> {
+        val response = api.verifyOtp(phoneNumber, otp)
         return response
     }
 }
