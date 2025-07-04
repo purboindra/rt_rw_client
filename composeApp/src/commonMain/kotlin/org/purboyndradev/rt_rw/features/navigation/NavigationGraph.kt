e8a77eea6ec51821d050475e97d7f9872095d8ea
purboyndra <purboindra@gmail.com>
2025-07-04 21:41:22 +0700

    feat: Pass phone number to OTPScreen

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/navigation/NavigationGraph.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/navigation/NavigationGraph.kt
@@ -5,6 +5,7 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import org.purboyndradev.rt_rw.features.auth.presentation.LoginScreen
 import org.purboyndradev.rt_rw.features.auth.presentation.OTPScreen
 
@@ -19,8 +20,13 @@ fun NavigationGraph(modifier: Modifier = Modifier) {
         composable<Login> {
             LoginScreen(navHostController = navController)
         }
-        composable<OTP> {
-            OTPScreen(navHostController = navController)
+        composable<OTP> { backStackEntry ->
+            val otpScreen = backStackEntry.toRoute<OTP>()
+            val phoneNumber = otpScreen.phoneNumber
+            OTPScreen(
+                navHostController = navController,
+                phoneNumber = phoneNumber
+            )
         }
     }
 }
