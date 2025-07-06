7e4ea19e1dc0e7b500da8b77ab71b630b13be4d2
purboyndra <purboindra@gmail.com>
2025-07-07 06:51:00 +0700

    feat: Implement SplashViewModel.kt

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/splash/SplashViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/splash/SplashViewModel.kt
@@ -2,16 +2,19 @@ package org.purboyndradev.rt_rw.features.splash
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
 import org.purboyndradev.rt_rw.core.data.datastore.UserRepository
 import org.purboyndradev.rt_rw.core.domain.Result
 import org.purboyndradev.rt_rw.domain.usecases.RefreshTokenUseCase
+
+sealed class SplashNavigationState {
+    data object Idle : SplashNavigationState()
+    data object NavigateToLogin : SplashNavigationState()
+    data object NavigateToHome : SplashNavigationState()
+}
 
 class SplashViewModel(
     private val refreshTokenUseCase: RefreshTokenUseCase,
@@ -21,43 +24,49 @@ class SplashViewModel(
     private val _isLoading: MutableStateFlow<Boolean> = MutableStateFlow(false)
     val isLoading = _isLoading.asStateFlow()
     
+    private val _navigationState =
+        MutableStateFlow<SplashNavigationState>(SplashNavigationState.Idle)
+    val navigationState = _navigationState.asStateFlow()
+    
+    fun onUpdateNavigationState(value: SplashNavigationState) {
+        _navigationState.value = value
+    }
+    
     fun refreshToken() {
         
         _isLoading.value = true
         
         viewModelScope.launch {
             
-            println("Coroutine launched. Attempting to collect user data soon.")
-            delay(300) // Experiment with values like 100ms, 300ms, 500ms
-            println("Delay finished. Collecting currentUserData.")
+            val userData = userRepository.currentUserData.firstOrNull()
             
-            userRepository.currentUserData.catch { e ->
-                println("Error reading user data: ${e.message}")
-                emit(null)
-            }.collect { userData ->
-                println("Current user data: $userData")
-                
-//                val result = refreshTokenUseCase.execute(
-//                    accessToken = userData?.accessToken ?: "",
-//                    refreshToken = userData?.refreshToken ?: ""
-//                )
-//
-//                when (result) {
-//                    is Result.Success -> {
-//                        println("Success refresh splash: $result")
-//                        userRepository.updateUserAccessToken(
-//                            newAccessToken = result.data.data?.accessToken.toString(),
-//                            newRefreshToken = result.data.data?.accessToken.toString(),
-//                        )
-//                    }
-//
-//                    is Result.Error -> {
-//                        userRepository.clearUserData()
-//                        println("Error refresh splash: $result")
-//                    }
-//                }
-                _isLoading.value = false
+            if (userData == null) {
+                onUpdateNavigationState(SplashNavigationState.NavigateToLogin)
+                return@launch
             }
+            
+            println("Current user data: $userData")
+            
+            when (val result = refreshTokenUseCase(
+                accessToken = userData?.accessToken ?: "",
+                refreshToken = userData?.refreshToken ?: ""
+            )) {
+                is Result.Success -> {
+                    userRepository.updateUserAccessToken(
+                        newAccessToken = result.data.accessToken,
+                        newRefreshToken = result.data.refreshToken,
+                    )
+                    onUpdateNavigationState(SplashNavigationState.NavigateToHome)
+                }
+                
+                is Result.Error -> {
+                    userRepository.clearUserData()
+                    onUpdateNavigationState(SplashNavigationState.NavigateToLogin)
+                    println("Error refresh splash: $result")
+                }
+            }
+            
+            _isLoading.value = false
         }
     }
     
