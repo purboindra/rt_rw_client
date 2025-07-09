dcb2473a5dcfedc4c6e837c1a246aa53992b91d2
purbo <purba.gumelar@intra.adr>
2025-07-09 13:59:25 +0700

    fix: add Any type constraint to ResponseDto

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/dto/ResponseDto.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/dto/ResponseDto.kt
@@ -3,7 +3,7 @@ package org.purboyndradev.rt_rw.core.data.dto
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ResponseDto<T>(
+data class ResponseDto<T : Any>(
     val code: String? = null,
     val message: String,
     val data: T? = null,
