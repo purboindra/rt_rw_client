bea366026e12d26dee86bd01b0de8ceb06f1b82a
purbo <purba.gumelar@intra.adr>
2025-07-09 13:52:36 +0700

    feat: ActivityRepositoryImpl.kt

---
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/repository/ActivityRepositoryImpl.kt
@@ -0,0 +1,73 @@
+package org.purboyndradev.rt_rw.core.data.repository
+
+import org.purboyndradev.rt_rw.core.data.dto.ActivityDto
+import org.purboyndradev.rt_rw.core.data.dto.ResponseDto
+import org.purboyndradev.rt_rw.core.data.remote.api.ActivityApi
+import org.purboyndradev.rt_rw.core.data.remote.params.CreateActivityParams
+import org.purboyndradev.rt_rw.core.data.remote.params.PaginationParams
+import org.purboyndradev.rt_rw.core.data.remote.params.QueryParams
+import org.purboyndradev.rt_rw.core.domain.DataError
+import org.purboyndradev.rt_rw.core.domain.Result
+import org.purboyndradev.rt_rw.core.domain.mapBoth
+import org.purboyndradev.rt_rw.domain.repository.ActivityRepository
+
+class ActivityRepositoryImpl(
+    private val activityApi: ActivityApi
+) : ActivityRepository {
+    override suspend fun createActivity(params: CreateActivityParams): Result<ResponseDto<Unit>, DataError.Remote> {
+        return activityApi.createActivity(
+            params
+        ).mapBoth(
+            onSuccess = { data ->
+                Result.Success(data)
+            },
+            onFailure = { error ->
+                Result.Error(error)
+            }
+        )
+    }
+
+    override suspend fun fetchAllActivities(
+        paginationParams: PaginationParams?,
+        queryParams: QueryParams?
+    ): Result<ResponseDto<List<ActivityDto>>, DataError.Remote> {
+        return activityApi.fetchAllActivities(
+            paginationParams, queryParams
+        ).mapBoth(
+            onSuccess = { data ->
+                Result.Success(data)
+            },
+            onFailure = { error ->
+                Result.Error(error)
+
+            }
+        )
+    }
+
+    override suspend
+    fun fetchActivityById(id: String): Result<ResponseDto<ActivityDto>, DataError.Remote> {
+        return activityApi.fetchActivityById(id).mapBoth(
+            onSuccess = { data -> Result.Success(data) },
+            onFailure = { error -> Result.Error(error) }
+        )
+    }
+
+    override suspend
+    fun deleteActivity(id: String): Result<ResponseDto<Unit>, DataError.Remote> {
+        return activityApi.deleteActivity(id).mapBoth(
+            onSuccess = { data -> Result.Success(data) },
+            onFailure = { error -> Result.Error(error) }
+        )
+    }
+
+    override suspend
+    fun editActivity(
+        id: String,
+        params: CreateActivityParams
+    ): Result<ResponseDto<Unit>, DataError.Remote> {
+        return activityApi.editActivity(id, params).mapBoth(
+            onSuccess = { data -> Result.Success(data) },
+            onFailure = { error -> Result.Error(error) }
+        )
+    }
+}
