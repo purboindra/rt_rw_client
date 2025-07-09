b84f370c6fff07cb3ed8ba3249a14411b5e104e9
purbo <purba.gumelar@intra.adr>
2025-07-09 13:53:28 +0700

    refactor: update BASE_URL to include /api/v1

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/helper/Config.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/helper/Config.kt
@@ -1,4 +1,4 @@
 package org.purboyndradev.rt_rw.helper
 
 //const val BASE_URL = "http://10.0.2.2:3000"
-const val BASE_URL = "http://192.168.1.4:3000"
+const val BASE_URL = "http://192.168.1.4:3000/api/v1"
