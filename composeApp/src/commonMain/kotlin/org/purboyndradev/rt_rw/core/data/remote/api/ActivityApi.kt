c4fa2ea2ff837e3047b47561d422cb468dd51697
purbo <purba.gumelar@intra.adr>
2025-07-09 13:51:04 +0700

    feat: ActivityApi.kt

---
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/remote/api/ActivityApi.kt
@@ -0,0 +1,24 @@
+package org.purboyndradev.rt_rw.core.data.remote.api
+
+import org.purboyndradev.rt_rw.core.data.dto.ActivityDto
+import org.purboyndradev.rt_rw.core.data.dto.ResponseDto
+import org.purboyndradev.rt_rw.core.data.remote.params.CreateActivityParams
+import org.purboyndradev.rt_rw.core.data.remote.params.PaginationParams
+import org.purboyndradev.rt_rw.core.data.remote.params.QueryParams
+import org.purboyndradev.rt_rw.core.domain.DataError
+import org.purboyndradev.rt_rw.core.domain.Result
+
+interface ActivityApi {
+    suspend fun createActivity(params: CreateActivityParams): Result<ResponseDto<Unit>, DataError.Remote>
+    suspend fun fetchAllActivities(
+        paginationParams: PaginationParams? = null,
+        queryParams: QueryParams? = null
+    ): Result<ResponseDto<List<ActivityDto>>, DataError.Remote>
+
+    suspend fun fetchActivityById(id: String): Result<ResponseDto<ActivityDto>, DataError.Remote>
+    suspend fun deleteActivity(id: String): Result<ResponseDto<Unit>, DataError.Remote>
+    suspend fun editActivity(
+        id: String,
+        params: CreateActivityParams
+    ): Result<ResponseDto<Unit>, DataError.Remote>
+}
