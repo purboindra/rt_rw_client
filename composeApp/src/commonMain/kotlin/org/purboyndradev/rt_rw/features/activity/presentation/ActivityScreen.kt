9de9a28f4ae561d39855bdac66a8d227dd6f321d
purboyndra <purboindra@gmail.com>
2025-07-11 05:52:49 +0700

    feat: ActivityScreen.kt

---
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/activity/presentation/ActivityScreen.kt
@@ -0,0 +1,25 @@
+package org.purboyndradev.rt_rw.features.activity.presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun ActivityScreen(
+    modifier: Modifier = Modifier,
+    navHostController: NavHostController
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Hello Activity Screen")
+    }
+}
