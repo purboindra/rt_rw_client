c7c1600933cd2e7e0e9c91a6db89072084283aa7
purboyndra <purboindra@gmail.com>
2025-07-11 05:53:03 +0700

    feat: NewsScreen.kt

---
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/news/presentation/NewsScreen.kt
@@ -0,0 +1,25 @@
+package org.purboyndradev.rt_rw.features.news.presentation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun NewsScreen(
+    modifier: Modifier = Modifier,
+    navHostController: NavHostController
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Hello News Screen")
+    }
+}
