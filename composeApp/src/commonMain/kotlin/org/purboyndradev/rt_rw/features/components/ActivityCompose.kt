78bf1b2ee5968eac9529fd50340083e874438aa5
purboyndra <purboindra@gmail.com>
2025-07-11 21:34:19 +0700

    fix: center text ActivityCompose

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/components/ActivityCompose.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/components/ActivityCompose.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
@@ -40,7 +41,8 @@ fun ActivityCompose(modifier: Modifier = Modifier) {
                     Box(
                         modifier = Modifier.height(102.dp).width(184.dp).padding(horizontal = 5.dp).clip(
                             RoundedCornerShape(size = 16.dp)
-                        ).background(Color.LightGray)
+                        ).background(Color.LightGray),
+                        contentAlignment = Alignment.Center
                     ) {
                         Text("Hello World: $it")
                     }
