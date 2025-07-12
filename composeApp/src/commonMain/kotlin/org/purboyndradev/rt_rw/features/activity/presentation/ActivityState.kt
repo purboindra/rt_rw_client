69db0cd0157125a85aeb1d79781d8f3a21c51944
purboyndra <purboindra@gmail.com>
2025-07-13 05:22:07 +0700

    feat: ActivityState.kt

---
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/activity/presentation/ActivityState.kt
@@ -0,0 +1,11 @@
+package org.purboyndradev.rt_rw.features.activity.presentation
+
+import androidx.compose.runtime.Stable
+import org.purboyndradev.rt_rw.core.domain.model.ActivityModel
+
+@Stable
+data class ActivityState(
+    val activities: List<ActivityModel>,
+    val error: String? = null,
+    val loading: Boolean = false
+)
