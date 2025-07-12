7de286b4f772294adc17bca62f39ab9b10a0a541
purboyndra <purboindra@gmail.com>
2025-07-13 05:23:28 +0700

    feat: Implement ActivityRepository.kt

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/repository/ActivityRepository.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/repository/ActivityRepository.kt
@@ -9,14 +9,14 @@ import org.purboyndradev.rt_rw.core.domain.DataError
 import org.purboyndradev.rt_rw.core.domain.Result
 
 interface ActivityRepository {
-    suspend fun createActivity(params: CreateActivityParams): org.purboyndradev.rt_rw.core.domain.Result<ResponseDto<Unit>, DataError.Remote>
+    suspend fun createActivity(params: CreateActivityParams): Result<ResponseDto<Unit>, DataError.Remote>
     suspend fun fetchAllActivities(
         paginationParams: PaginationParams? = null,
         queryParams: QueryParams? = null
-    ): org.purboyndradev.rt_rw.core.domain.Result<ResponseDto<List<ActivityDto>>, DataError.Remote>
+    ): Result<ResponseDto<List<ActivityDto>>, DataError.Remote>
 
-    suspend fun fetchActivityById(id: String): org.purboyndradev.rt_rw.core.domain.Result<ResponseDto<ActivityDto>, DataError.Remote>
-    suspend fun deleteActivity(id: String): org.purboyndradev.rt_rw.core.domain.Result<ResponseDto<Unit>, DataError.Remote>
+    suspend fun fetchActivityById(id: String): Result<ResponseDto<ActivityDto>, DataError.Remote>
+    suspend fun deleteActivity(id: String): Result<ResponseDto<Unit>, DataError.Remote>
     suspend fun editActivity(
         id: String,
         params: CreateActivityParams
