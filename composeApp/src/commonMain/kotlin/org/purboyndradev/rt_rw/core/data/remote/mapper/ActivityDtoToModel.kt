63324da5480e65ef6982b359330d864901889a2f
purboyndra <purboindra@gmail.com>
2025-07-13 05:23:15 +0700

    feat: Add UserDBModel to ActivityModel

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/remote/mapper/ActivityDtoToModel.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/remote/mapper/ActivityDtoToModel.kt
@@ -2,6 +2,7 @@ package org.purboyndradev.rt_rw.core.data.remote.mapper
 
 import org.purboyndradev.rt_rw.core.data.dto.ActivityDto
 import org.purboyndradev.rt_rw.core.domain.model.ActivityModel
+import org.purboyndradev.rt_rw.domain.model.UserDBModel
 
 fun ActivityDto.toActivityModel(): ActivityModel {
     return ActivityModel(
@@ -10,5 +11,12 @@ fun ActivityDto.toActivityModel(): ActivityModel {
         description = description,
         rt = "",
         date = date,
+        pic = UserDBModel(
+            accessToken = "",
+            refreshToken = "",
+            username = "",
+            email = "",
+        ),
+        type = ""
     )
 }
