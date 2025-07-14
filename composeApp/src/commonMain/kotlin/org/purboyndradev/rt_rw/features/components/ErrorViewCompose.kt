3ff53947dff1d7eff4d0727a7ada18adc8af7915
purboyndra <purboindra@gmail.com>
2025-07-15 05:46:42 +0700

    feat: Implement ErrorViewCompose.kt

---
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/components/ErrorViewCompose.kt
@@ -0,0 +1,19 @@
+package org.purboyndradev.rt_rw.features.components
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun ErrorViewCompose(modifier: Modifier = Modifier, errorMessage: String) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(errorMessage)
+    }
+}
