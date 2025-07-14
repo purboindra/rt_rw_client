05ededa06cb98ce3700ec7d4116d8785369de091
purboyndra <purboindra@gmail.com>
2025-07-15 06:38:49 +0700

    feat: Add "Add" button to ActivityScreen.kt

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/activity/presentation/ActivityScreen.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/activity/presentation/ActivityScreen.kt
@@ -1,16 +1,31 @@
 package org.purboyndradev.rt_rw.features.activity.presentation
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
+import compose.icons.FeatherIcons
+import compose.icons.feathericons.Plus
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.parameter.parametersOf
 import org.purboyndradev.rt_rw.core.domain.model.ActivityModel
@@ -32,21 +47,42 @@ fun ActivityScreen(
         activityViewModel.fetchActivities()
     }
     
-    when {
-        activityState.loading -> {
-            LoadingIndicatorCompose(modifier)
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Row(
+            modifier = modifier.fillMaxWidth().padding(
+                horizontal = 18.dp
+            ),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text("Activities", style = MaterialTheme.typography.titleMedium)
+            IconButton(onClick = {}) {
+                Icon(
+                    FeatherIcons.Plus, contentDescription = "Add",
+                    tint = Color.Black
+                )
+            }
         }
-        
-        activityState.error != null -> {
-            ErrorViewCompose(modifier, activityState.error ?: "Unknown Error")
-        }
-        
-        activityState.activities.isEmpty() -> {
-            EmptyStateView(modifier, "No Activity Found")
-        }
-        
-        else -> {
-            ActivityList(modifier, activityState.activities)
+        when {
+            activityState.loading -> {
+                LoadingIndicatorCompose(modifier)
+            }
+            
+            activityState.error != null -> {
+                ErrorViewCompose(
+                    modifier,
+                    activityState.error ?: "Unknown Error"
+                )
+            }
+            
+            activityState.activities.isEmpty() -> {
+                EmptyStateView(modifier, "No Activity Found")
+            }
+            
+            else -> {
+                ActivityList(modifier, activityState.activities)
+            }
         }
     }
 }
