ba0d6e63d73c3a1b074d55482668d04959afb050
purboyndra <purboindra@gmail.com>
2025-07-17 06:50:23 +0700

    feat: DateHelper.kt

---
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/helper/DateHelper.kt
@@ -0,0 +1,48 @@
+package org.purboyndradev.rt_rw.helper
+
+import kotlinx.datetime.*
+import kotlin.time.ExperimentalTime
+import kotlinx.datetime.format.*
+import kotlin.time.Instant
+
+class DateHelper {
+    companion object {
+        
+        @OptIn(FormatStringsInDatetimeFormats::class)
+        private val dateTimeFormatFinal = LocalDateTime.Format {
+            byUnicodePattern("ddd, dd MMM yyyy'T'HH:mm")
+        }
+        
+        private val dateTimeFormat = LocalDateTime.Format {
+            year()
+            char('-')
+            monthNumber(padding = Padding.ZERO)
+            char('-')
+            dayOfMonth(padding = Padding.ZERO)
+            char(' ')
+            hour(padding = Padding.ZERO)
+            char(':')
+            minute(padding = Padding.ZERO)
+            char(':')
+            second(padding = Padding.ZERO)
+        }
+        
+        @OptIn(ExperimentalTime::class)
+        fun convertEpochToDate(epochTime: Int): String {
+            
+            val epochMillis =
+                if (epochTime.toString().length <= 10) {
+                    epochTime * 1000L
+                } else {
+                    epochTime.toLong()
+                }
+            
+            val instant = Instant.fromEpochMilliseconds(epochMillis)
+            val localDateTime =
+                instant.toLocalDateTime(TimeZone.currentSystemDefault())
+            
+            val formatted = localDateTime.format(dateTimeFormat)
+            return formatted
+        }
+    }
+}
