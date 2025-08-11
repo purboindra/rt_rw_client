5589666d0812c31848a3e6d67b94a8e5536eb70b
purboyndra <purboindra@gmail.com>
2025-08-12 06:14:56 +0700

    feat: request permission

--- composeApp/src/androidMain/kotlin/org/purboyndradev/rt_rw/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/purboyndradev/rt_rw/MainActivity.kt
@@ -1,20 +1,58 @@
 package org.purboyndradev.rt_rw
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 
 class MainActivity : ComponentActivity() {
+    
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission(),
+    ) { isGranted ->
+        if (isGranted) {
+            // TODO: DO SOMETHING
+        } else {
+            /// TODO: DO SOMETHING
+        }
+    }
+    
     override fun onCreate(savedInstanceState: Bundle?) {
+        
+        askNotificationPermission()
+        
+        FirebaseHelper.getTokenFCM { token -> println("FCM Token: $token") }
+        
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
             App()
         }
     }
+    
+    private fun askNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                /// TODO: DO SOMETHING
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
+                /// TODO: DO SOMETHING
+            } else {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+    }
+    
 }
 
 @Preview
