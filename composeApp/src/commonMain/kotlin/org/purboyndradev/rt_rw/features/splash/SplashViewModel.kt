10accec985c29a82097e46c8fdc038efe73c7eb1
purboyndra <purboindra@gmail.com>
2025-08-17 04:05:30 +0700

    feat: hasAuthenticated for check is user already authenticated or not

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/splash/SplashViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/splash/SplashViewModel.kt
@@ -2,6 +2,7 @@ package org.purboyndradev.rt_rw.features.splash
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import io.ktor.util.date.getTimeMillis
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.first
@@ -13,6 +14,7 @@ import org.purboyndradev.rt_rw.core.data.datastore.NotificationRepository
 import org.purboyndradev.rt_rw.core.domain.Result
 import org.purboyndradev.rt_rw.domain.usecases.RefreshTokenUseCase
 import org.purboyndradev.rt_rw.features.navigation.StartDestinationData
+import org.purboyndradev.rt_rw.helper.JWTObject
 
 sealed class SplashNavigationState {
     data object Idle : SplashNavigationState()
@@ -51,7 +53,7 @@ class SplashViewModel(
                 _isLoading.value = false
                 return@launch
             }
-
+            
             val deniedAt: Long? = deniedAtFlow.first()
             
             val shouldPrompt = when {
@@ -67,6 +69,26 @@ class SplashViewModel(
             
             _isLoading.value = false
         }
+    }
+    
+    suspend fun hasAuthenticated(): Boolean {
+        val accessToken = appAuthRepository.accessTokenFlow.firstOrNull()
+        val refreshToken = appAuthRepository.refreshTokenFlow.firstOrNull()
+        
+        if (accessToken.isNullOrBlank() || refreshToken.isNullOrBlank()) {
+            return false
+        }
+        
+        val payload = JWTObject.decodeJwtPayload(accessToken)
+        
+        println("Payload hasAuthenticated: $payload")
+        
+        val expSeconds = payload
+            ?.get("exp")?.toString()?.toLongOrNull()
+            ?: return false
+        
+        val nowSeconds = getTimeMillis() / 1000
+        return expSeconds > nowSeconds
     }
     
     
