aa62b3d64b32150034ea6b1ef2bde8847f5fc53d
purboindra <purboindra@gmail.com>
2025-09-22 21:59:45 +0700

    feat: add NewsDtoToModel mapper

---
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/remote/mapper/NewsDtoToModel.kt
@@ -0,0 +1,15 @@
+package org.purboyndradev.rt_rw.core.data.remote.mapper
+
+import org.purboyndradev.rt_rw.core.data.dto.NewsDto
+import org.purboyndradev.rt_rw.core.domain.model.NewsModel
+
+fun NewsDto.toNewsModel(): NewsModel {
+    return NewsModel(
+        id = id,
+        title = title,
+        description = description,
+        body = body,
+        rt = rt,
+        user = user
+    )
+}
