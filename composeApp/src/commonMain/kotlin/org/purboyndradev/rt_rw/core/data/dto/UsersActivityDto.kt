1ddad7612710494507a82fa59af1f68698abf647
purboindra <purboindra@gmail.com>
2025-10-09 06:35:02 +0700

    feat: UsersActivityDto

---
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/dto/UsersActivityDto.kt
@@ -0,0 +1,17 @@
+package org.purboyndradev.rt_rw.core.data.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UsersActivityDto(
+    val id: String,
+    val name: String,
+    val phone: String,
+    val email: String?,
+    val role: String,
+    val address: String,
+    val rtId: String,
+    val isVerified: Boolean,
+    val emailVerified: Boolean,
+    val image: String?,
+)
