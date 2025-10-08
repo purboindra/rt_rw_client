07dddf1394eeb3834cb57a7cc251606b6f0540da
purboindra <purboindra@gmail.com>
2025-10-09 06:34:16 +0700

    feat: fetchUsersActivity KtorActivityRemoteDatasource.kt

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/remote/impl/KtorActivityRemoteDatasource.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/remote/impl/KtorActivityRemoteDatasource.kt
@@ -1,15 +1,18 @@
 package org.purboyndradev.rt_rw.core.data.remote.impl
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
 import io.ktor.client.request.delete
 import io.ktor.client.request.get
 import io.ktor.client.request.post
 import io.ktor.client.request.put
 import io.ktor.client.request.setBody
 import io.ktor.http.appendPathSegments
+import io.ktor.http.cio.Response
 import org.purboyndradev.rt_rw.core.data.dto.ActivityDetailDto
 import org.purboyndradev.rt_rw.core.data.dto.ActivityDto
 import org.purboyndradev.rt_rw.core.data.dto.ResponseDto
+import org.purboyndradev.rt_rw.core.data.dto.UsersActivityDto
 import org.purboyndradev.rt_rw.core.data.remote.api.ActivityApi
 import org.purboyndradev.rt_rw.core.data.remote.params.CreateActivityParams
 import org.purboyndradev.rt_rw.core.data.remote.params.JoinActivityParams
@@ -22,92 +25,91 @@ import org.purboyndradev.rt_rw.core.network.safeCallWrapped
 class KtorActivityRemoteDatasource(private val httpClient: HttpClient) :
     ActivityApi {
     override suspend fun createActivity(params: CreateActivityParams): Result<ResponseDto<Unit>, AppError> {
-        return safeCallWrapped(
-            call = {
-                httpClient.post {
-                    url {
-                        appendPathSegments("api/v1/activities")
-                    }
-                    setBody(params)
+        return safeCallWrapped {
+            httpClient.post {
+                url {
+                    appendPathSegments("api/v1/activities")
                 }
-            }
-        )
+                setBody(params)
+            }.body<ResponseDto<Unit>>()
+        }
     }
-    
+
     override suspend fun fetchAllActivities(
         paginationParams: PaginationParams?,
         queryParams: QueryParams?
     ): Result<ResponseDto<List<ActivityDto>>, AppError> {
-        return safeCallWrapped(
-            call = {
-                httpClient.get {
-                    url {
-                        appendPathSegments("api/v1/activities")
-                        paginationParams?.let {
-                            parameters.append("page", it.page.toString())
-                            parameters.append("limit", it.limit.toString())
-                        }
-                        queryParams?.let {
-                            parameters.append("query", it.query)
-                        }
+        return safeCallWrapped {
+            httpClient.get {
+                url {
+                    appendPathSegments("api/v1/activities")
+                    paginationParams?.let {
+                        parameters.append("page", it.page.toString())
+                        parameters.append("limit", it.limit.toString())
+                    }
+                    queryParams?.let {
+                        parameters.append("query", it.query)
                     }
                 }
-            }
-        )
+            }.body<ResponseDto<List<ActivityDto>>>()
+        }
     }
-    
+
     override suspend fun fetchActivityById(id: String): Result<ResponseDto<ActivityDetailDto>, AppError> {
-        return safeCallWrapped(
-            call = {
-                httpClient.get {
-                    url {
-                        appendPathSegments("api/v1/activities/$id")
-                    }
+        return safeCallWrapped {
+            httpClient.get {
+                url {
+                    appendPathSegments("api/v1/activities/$id")
                 }
-            }
-        )
+            }.body<ResponseDto<ActivityDetailDto>>()
+        }
+
     }
-    
+
     override suspend fun deleteActivity(id: String): Result<ResponseDto<Unit>, AppError> {
-        return safeCallWrapped(
-            call = {
-                httpClient.delete {
-                    url {
-                        appendPathSegments("api/v1/activities/$id")
-                    }
+        return safeCallWrapped {
+            httpClient.delete {
+                url {
+                    appendPathSegments("api/v1/activities/$id")
                 }
-            }
-        )
+            }.body<ResponseDto<Unit>>()
+        }
     }
-    
+
     override suspend fun editActivity(
         id: String,
         params: CreateActivityParams
     ): Result<ResponseDto<Unit>, AppError> {
-        return safeCallWrapped(
-            call = {
-                httpClient.put {
-                    url {
-                        appendPathSegments("api/v1/activities/$id")
-                    }
-                    setBody(params)
+        return safeCallWrapped {
+            httpClient.put {
+                url {
+                    appendPathSegments("api/v1/activities/$id")
                 }
-            }
-        )
+                setBody(params)
+            }.body<ResponseDto<Unit>>()
+        }
     }
-    
+
     override suspend fun joinActivity(params: JoinActivityParams): Result<ResponseDto<Unit>, AppError> {
-        return safeCallWrapped(
-            call = {
-                httpClient.post {
-                    url {
-                        appendPathSegments("api/v1/activities/join")
-                    }
-                    setBody(
-                        params
-                    )
+        return safeCallWrapped {
+            httpClient.post {
+                url {
+                    appendPathSegments("api/v1/activities/join")
                 }
-            }
-        )
+                setBody(
+                    params
+                )
+            }.body<ResponseDto<Unit>>()
+        }
+    }
+
+    override suspend fun fetchUsersActivity(id: String): Result<ResponseDto<List<UsersActivityDto>>, AppError> {
+        return safeCallWrapped {
+            httpClient.get {
+                url {
+                    appendPathSegments("api/v1/activities/${id}/users")
+                }
+            }.body<ResponseDto<List<UsersActivityDto>>>()
+        }
     }
 }
