ae6398688a2c117b28b0ff625dd7b41671c81dea
purboindra <purboindra@gmail.com>
2025-11-09 05:01:55 +0700

    feat: update activity repository method signatures and add mappers

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/repository/ActivityRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/repository/ActivityRepositoryImpl.kt
@@ -5,6 +5,8 @@ import org.purboyndradev.rt_rw.core.data.dto.ActivityDto
 import org.purboyndradev.rt_rw.core.data.dto.ResponseDto
 import org.purboyndradev.rt_rw.core.data.dto.UsersActivityDto
 import org.purboyndradev.rt_rw.core.data.remote.api.ActivityApi
+import org.purboyndradev.rt_rw.core.data.remote.mapper.toActivityDetailModel
+import org.purboyndradev.rt_rw.core.data.remote.mapper.toActivityModel
 import org.purboyndradev.rt_rw.core.data.remote.params.CreateActivityParams
 import org.purboyndradev.rt_rw.core.data.remote.params.JoinActivityParams
 import org.purboyndradev.rt_rw.core.data.remote.params.PaginationParams
@@ -12,13 +14,15 @@ import org.purboyndradev.rt_rw.core.data.remote.params.QueryParams
 import org.purboyndradev.rt_rw.core.domain.AppError
 import org.purboyndradev.rt_rw.core.domain.Result
 import org.purboyndradev.rt_rw.core.domain.mapBoth
+import org.purboyndradev.rt_rw.core.domain.model.ActivityDetailModel
+import org.purboyndradev.rt_rw.core.domain.model.ActivityModel
 import org.purboyndradev.rt_rw.domain.repository.ActivityRepository
 
 class ActivityRepositoryImpl(
     private val activityApi: ActivityApi
 ) : ActivityRepository {
-    override suspend fun createActivity(params: CreateActivityParams): ResponseDto<Unit> {
-        return activityApi.createActivity(
+    override suspend fun createActivity(params: CreateActivityParams) {
+        activityApi.createActivity(
             params
         )
     }
@@ -26,15 +30,22 @@ class ActivityRepositoryImpl(
     override suspend fun fetchAllActivities(
         paginationParams: PaginationParams?,
         queryParams: QueryParams?
-    ): ResponseDto<List<ActivityDto>> {
-        return activityApi.fetchAllActivities(
+    ): List<ActivityModel> {
+        val result = activityApi.fetchAllActivities(
             paginationParams, queryParams
         )
+        return result.data?.map {
+            it.toActivityModel()
+        } ?: emptyList()
     }
 
     override suspend
-    fun fetchActivityById(id: String): ResponseDto<ActivityDetailDto> {
-        return activityApi.fetchActivityById(id)
+    fun fetchActivityById(id: String): ActivityDetailModel {
+        val result=  activityApi.fetchActivityById(id)
+        if(result.data != null){
+            return result.data.toActivityDetailModel()
+        }
+        throw Exception()
     }
 
     override suspend
