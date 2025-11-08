aadfcc6ef4a656cbb0618ab8308ee70270b084e0
purboindra <purboindra@gmail.com>
2025-11-09 05:24:02 +0700

    refactor: update BannerRepository return type

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/repository/BannerRepository.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/repository/BannerRepository.kt
@@ -1,16 +1,12 @@
 package org.purboyndradev.rt_rw.domain.repository
 
-import org.purboyndradev.rt_rw.core.data.dto.ActivityDto
-import org.purboyndradev.rt_rw.core.data.dto.BannerDto
-import org.purboyndradev.rt_rw.core.data.dto.ResponseDto
 import org.purboyndradev.rt_rw.core.data.remote.params.PaginationParams
 import org.purboyndradev.rt_rw.core.data.remote.params.QueryParams
-import org.purboyndradev.rt_rw.core.domain.AppError
-import org.purboyndradev.rt_rw.core.domain.Result
+import org.purboyndradev.rt_rw.core.domain.model.BannerModel
 
 interface BannerRepository {
     suspend fun fetchAllBanners(
         paginationParams: PaginationParams? = null,
         queryParams: QueryParams? = null
-    ): ResponseDto<List<BannerDto>>
+    ): List<BannerModel>
 }
