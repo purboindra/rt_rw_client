55b5eab4fb6d84887d734f3a1ba5c1befff97117
purboindra <purboindra@gmail.com>
2025-11-09 05:09:51 +0700

    refactor: simplify FetchUsersActivityUseCase

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/FetchUsersActivityUseCase.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/FetchUsersActivityUseCase.kt
@@ -1,6 +1,5 @@
 package org.purboyndradev.rt_rw.domain.usecases
 
-import org.purboyndradev.rt_rw.core.data.remote.mapper.toUsersActivityModel
 import org.purboyndradev.rt_rw.core.domain.AppError
 import org.purboyndradev.rt_rw.core.domain.Result
 import org.purboyndradev.rt_rw.core.domain.model.UsersActivityModel
@@ -10,12 +9,8 @@ import org.purboyndradev.rt_rw.domain.repository.ActivityRepository
 class FetchUsersActivityUseCase(private val activityRepository: ActivityRepository) {
     suspend operator fun invoke(id: String): Result<List<UsersActivityModel>, AppError> {
         return try {
-            val result = activityRepository.fetchUsersActivity(id)
-            val data = result.data ?: return Result.Error(AppError.Remote.NotFound)
-            val usersActivity = data.map {
-                it.toUsersActivityModel()
-            }
-            Result.Success(usersActivity)
+            val users = activityRepository.fetchUsersActivity(id)
+            Result.Success(users)
         } catch (e: Exception) {
             Result.Error(mapKtorExceptionToAppError(e))
         }
