a7f29a9b7faa9e843b8ea71ade096c4afa30b29b
purboindra <purboindra@gmail.com>
2025-11-09 05:24:07 +0700

    refactor: remove unnecessary banner mapping

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/FetchAllBannersUseCase.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/FetchAllBannersUseCase.kt
@@ -1,6 +1,5 @@
 package org.purboyndradev.rt_rw.domain.usecases
 
-import org.purboyndradev.rt_rw.core.data.remote.mapper.toBannerModel
 import org.purboyndradev.rt_rw.core.domain.AppError
 import org.purboyndradev.rt_rw.core.domain.Result
 import org.purboyndradev.rt_rw.core.domain.model.BannerModel
@@ -11,11 +10,7 @@ class FetchAllBannersUseCase(private val bannerRepository: BannerRepository) {
     suspend operator fun invoke(): Result<List<BannerModel>, AppError> {
         return try {
             val result = bannerRepository.fetchAllBanners()
-            val data = result.data ?: return Result.Error(AppError.Remote.NotFound)
-            var banners: List<BannerModel> = emptyList()
-            data.forEach {
-                banners = banners + it.toBannerModel()
-            }
+            val banners = result
             Result.Success(banners)
         } catch (e: Exception) {
             Result.Error(mapKtorExceptionToAppError(e))
