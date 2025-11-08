8d2404c7a4cbd1005fdc460dd89afd2c802d6506
purboindra <purboindra@gmail.com>
2025-11-09 05:16:58 +0700

    feat: add SignInModel data class

---
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/domain/model/SignInModel.kt
@@ -0,0 +1,14 @@
+package org.purboyndradev.rt_rw.core.domain.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SignInModel(
+    @SerialName("redirect_url")
+    val redirectUrl: String? = null,
+    @SerialName("access_token")
+    val accessToken: String = "",
+    @SerialName("refresh_token")
+    val refreshToken: String = "",
+)
