43b5c678d2e61be1f59af997d0241ebd5fbf1585
purboindra <purboindra@gmail.com>
2025-11-09 04:46:18 +0700

    feat: ReportDto.kt

---
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/dto/ReportDto.kt
@@ -0,0 +1,32 @@
+package org.purboyndradev.rt_rw.core.data.dto
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import org.purboyndradev.rt_rw.core.domain.model.UserModel
+
+@Serializable
+enum class ReportStatus {
+    @SerialName("OPEN")
+    OPEN,
+
+    @SerialName("IN_PROGRESS")
+    IN_PROGRESS,
+
+    @SerialName("CLOSED")
+    CLOSED,
+
+    @SerialName("RESOLVED")
+    RESOLVED
+}
+
+@Serializable
+data class ReportDto(
+    val id: String,
+    val title: String,
+    val createdAt: String,
+    val rtId: String,
+    val imageUrl: String,
+    val description: String,
+    val status: ReportStatus,
+    val user: UserModel
+)
