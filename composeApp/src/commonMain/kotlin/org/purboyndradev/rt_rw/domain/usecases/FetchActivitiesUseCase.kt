27bc2f90e427c6532595a076aa7de763c870fd9f
purboindra <purboindra@gmail.com>
2025-11-09 05:09:38 +0700

    refactor: remove mapper in FetchActivitiesUseCase

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/FetchActivitiesUseCase.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/FetchActivitiesUseCase.kt
@@ -1,6 +1,5 @@
 package org.purboyndradev.rt_rw.domain.usecases
 
-import org.purboyndradev.rt_rw.core.data.remote.mapper.toActivityModel
 import org.purboyndradev.rt_rw.core.domain.AppError
 import org.purboyndradev.rt_rw.core.domain.Result
 import org.purboyndradev.rt_rw.core.domain.model.ActivityModel
@@ -10,11 +9,7 @@ import org.purboyndradev.rt_rw.domain.repository.ActivityRepository
 class FetchActivitiesUseCase(private val activityRepository: ActivityRepository) {
     suspend operator fun invoke(): Result<List<ActivityModel>, AppError> {
         return try {
-            val response = activityRepository.fetchAllActivities()
-            val data = response.data ?: return Result.Error(AppError.Remote.NotFound)
-            val activities = data.map {
-                it.toActivityModel()
-            }
+            val activities = activityRepository.fetchAllActivities()
             Result.Success(activities)
         } catch (e: Exception) {
             Result.Error(mapKtorExceptionToAppError(e))
