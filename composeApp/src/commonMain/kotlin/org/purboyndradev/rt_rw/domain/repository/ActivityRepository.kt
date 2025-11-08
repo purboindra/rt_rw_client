bd180c62ed3587406cd2534a3cc2e8c2ca516121
purboindra <purboindra@gmail.com>
2025-11-09 04:48:36 +0700

    feat: refactor ActivityRepository interface

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/repository/ActivityRepository.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/repository/ActivityRepository.kt
@@ -1,35 +1,33 @@
 package org.purboyndradev.rt_rw.domain.repository
 
-import org.purboyndradev.rt_rw.core.data.dto.ActivityDetailDto
-import org.purboyndradev.rt_rw.core.data.dto.ActivityDto
 import org.purboyndradev.rt_rw.core.data.dto.ResponseDto
-import org.purboyndradev.rt_rw.core.data.dto.UsersActivityDto
 import org.purboyndradev.rt_rw.core.data.remote.params.CreateActivityParams
 import org.purboyndradev.rt_rw.core.data.remote.params.JoinActivityParams
 import org.purboyndradev.rt_rw.core.data.remote.params.PaginationParams
 import org.purboyndradev.rt_rw.core.data.remote.params.QueryParams
-import org.purboyndradev.rt_rw.core.domain.AppError
-import org.purboyndradev.rt_rw.core.domain.Result
+import org.purboyndradev.rt_rw.core.domain.model.ActivityDetailModel
+import org.purboyndradev.rt_rw.core.domain.model.ActivityModel
+import org.purboyndradev.rt_rw.core.domain.model.UsersActivityModel
 
 interface ActivityRepository {
     suspend fun createActivity(params: CreateActivityParams): ResponseDto<Unit>
     suspend fun fetchAllActivities(
         paginationParams: PaginationParams? = null,
         queryParams: QueryParams? = null
-    ): ResponseDto<List<ActivityDto>>
+    ): List<ActivityModel>
 
-    suspend fun fetchActivityById(id: String): ResponseDto<ActivityDetailDto>
-    suspend fun deleteActivity(id: String): ResponseDto<Unit>
+    suspend fun fetchActivityById(id: String): ActivityDetailModel
+    suspend fun deleteActivity(id: String)
     suspend fun editActivity(
         id: String,
         params: CreateActivityParams
-    ): ResponseDto<Unit>
+    )
 
     suspend fun joinActivity(
         params: JoinActivityParams
-    ): ResponseDto<Unit>
+    )
 
     suspend fun fetchUsersActivity(
         id: String
-    ): ResponseDto<List<UsersActivityDto>>
+    ): List<UsersActivityModel>
 }
