9494051be802f3c852d81edb200aa7e5a50f8b68
purboindra <purboindra@gmail.com>
2025-11-09 05:25:16 +0700

    feat: refactor NewsRepository to return domain models

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/repository/NewsRepository.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/repository/NewsRepository.kt
@@ -1,16 +1,15 @@
 package org.purboyndradev.rt_rw.domain.repository
 
-import org.purboyndradev.rt_rw.core.data.dto.NewsDto
-import org.purboyndradev.rt_rw.core.data.dto.ResponseDto
 import org.purboyndradev.rt_rw.core.data.remote.params.PaginationParams
 import org.purboyndradev.rt_rw.core.data.remote.params.QueryParams
+import org.purboyndradev.rt_rw.core.domain.model.NewsModel
 
 interface NewsRepository {
     suspend fun fetchAllNews(
         paginationParams: PaginationParams? = null,
         queryParams: QueryParams? = null
-    ): ResponseDto<List<NewsDto>>
+    ): List<NewsModel>
 
-    suspend fun fetchNewsById(id: String): ResponseDto<NewsDto>
+    suspend fun fetchNewsById(id: String): NewsModel
 
 }
