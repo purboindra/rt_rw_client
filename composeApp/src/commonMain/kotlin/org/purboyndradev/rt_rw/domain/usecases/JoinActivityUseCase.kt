23b0dfd3e976ccf747b704e41295b8c507f74511
purboindra <purboindra@gmail.com>
2025-11-09 05:09:55 +0700

    refactor: simplify JoinActivityUseCase response handling

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/JoinActivityUseCase.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/JoinActivityUseCase.kt
@@ -9,9 +9,8 @@ import org.purboyndradev.rt_rw.domain.repository.ActivityRepository
 class JoinActivityUseCase(private val activityRepository: ActivityRepository) {
     suspend operator fun invoke(params: JoinActivityParams): Result<Unit, AppError> {
         return try {
-            val result = activityRepository.joinActivity(params)
-            val data = result.data ?: return Result.Error(AppError.Remote.NotFound)
-            Result.Success(data)
+            activityRepository.joinActivity(params)
+            Result.Success(Unit)
         } catch (e: Exception) {
             Result.Error(mapKtorExceptionToAppError(e))
         }
