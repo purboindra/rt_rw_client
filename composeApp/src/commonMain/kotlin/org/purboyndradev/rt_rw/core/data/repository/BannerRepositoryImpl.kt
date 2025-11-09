aad3581b237fa1078ed10de6fb38c4dcf7e4da7f
purboindra <purboindra@gmail.com>
2025-11-09 08:00:17 +0700

    feat: add banner data mapper

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/repository/BannerRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/repository/BannerRepositoryImpl.kt
@@ -1,20 +1,24 @@
 package org.purboyndradev.rt_rw.core.data.repository
 
-import org.purboyndradev.rt_rw.core.data.dto.BannerDto
-import org.purboyndradev.rt_rw.core.data.dto.ResponseDto
 import org.purboyndradev.rt_rw.core.data.remote.api.BannerApi
+import org.purboyndradev.rt_rw.core.data.remote.mapper.toBannerModel
 import org.purboyndradev.rt_rw.core.data.remote.params.PaginationParams
 import org.purboyndradev.rt_rw.core.data.remote.params.QueryParams
+import org.purboyndradev.rt_rw.core.domain.model.BannerModel
 import org.purboyndradev.rt_rw.domain.repository.BannerRepository
 
 class BannerRepositoryImpl(private val bannerApi: BannerApi) : BannerRepository {
     override suspend fun fetchAllBanners(
         paginationParams: PaginationParams?,
         queryParams: QueryParams?
-    ): ResponseDto<List<BannerDto>> {
-        return bannerApi.fetchAllBanners(
+    ): List<BannerModel> {
+        val result = bannerApi.fetchAllBanners(
             paginationParams = paginationParams,
             queryParams = queryParams,
         )
+        val banners = result.data?.map {
+            it.toBannerModel()
+        } ?: emptyList()
+        return banners
     }
 }
