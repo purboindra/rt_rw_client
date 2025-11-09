bb2e8b48c696d94d772e1c6159653990c1f00f44
purboindra <purboindra@gmail.com>
2025-11-09 08:02:36 +0700

    feat: update NewsRepositoryImpl to return domain model

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/repository/NewsRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/repository/NewsRepositoryImpl.kt
@@ -1,21 +1,28 @@
 package org.purboyndradev.rt_rw.core.data.repository
 
-import org.purboyndradev.rt_rw.core.data.dto.NewsDto
-import org.purboyndradev.rt_rw.core.data.dto.ResponseDto
 import org.purboyndradev.rt_rw.core.data.remote.api.NewsApi
+import org.purboyndradev.rt_rw.core.data.remote.mapper.toNewsModel
 import org.purboyndradev.rt_rw.core.data.remote.params.PaginationParams
 import org.purboyndradev.rt_rw.core.data.remote.params.QueryParams
+import org.purboyndradev.rt_rw.core.domain.model.NewsModel
+import org.purboyndradev.rt_rw.core.network.DataNotFoundException
 import org.purboyndradev.rt_rw.domain.repository.NewsRepository
 
 class NewsRepositoryImpl(private val newsApi: NewsApi) : NewsRepository {
     override suspend fun fetchAllNews(
         paginationParams: PaginationParams?,
         queryParams: QueryParams?
-    ): ResponseDto<List<NewsDto>> {
-        return newsApi.fetchAllNews(paginationParams, queryParams)
+    ): List<NewsModel> {
+        val result = newsApi.fetchAllNews(paginationParams, queryParams)
+        val allNews = result.data?.map {
+            it.toNewsModel()
+        } ?: emptyList()
+        return allNews
     }
 
-    override suspend fun fetchNewsById(id: String): ResponseDto<NewsDto> {
-        return newsApi.fetchNewsById(id)
+    override suspend fun fetchNewsById(id: String): NewsModel {
+        val result = newsApi.fetchNewsById(id)
+        val news = result.data?.toNewsModel()
+        return news ?: throw DataNotFoundException("News not found")
     }
 }
