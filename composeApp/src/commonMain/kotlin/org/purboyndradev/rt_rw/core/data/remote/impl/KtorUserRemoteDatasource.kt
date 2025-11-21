4da490dcb34aa186944b512fd2818b0d66545aa4
purboindra <purboindra@gmail.com>
2025-11-22 05:11:48 +0700

    feat: KtorUserRemoteDatasource.kt

---
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/remote/impl/KtorUserRemoteDatasource.kt
@@ -0,0 +1,35 @@
+package org.purboyndradev.rt_rw.core.data.remote.impl
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.appendPathSegments
+import org.purboyndradev.rt_rw.core.data.dto.ResponseDto
+import org.purboyndradev.rt_rw.core.data.remote.api.UserApi
+import org.purboyndradev.rt_rw.core.data.remote.params.RequestEmailVerificationParams
+import org.purboyndradev.rt_rw.core.data.remote.params.VerifyEmailParams
+
+class KtorUserRemoteDatasource(
+    private val httpClient: HttpClient
+) : UserApi {
+    override suspend fun requestEmailVerification(params: RequestEmailVerificationParams): ResponseDto<Unit> {
+        return httpClient.post {
+            url {
+                appendPathSegments("api/v1/users/request-email-verification")
+            }
+            setBody(params)
+        }.body<ResponseDto<Unit>>()
+    }
+
+    override suspend fun verifyEmail(
+        params: VerifyEmailParams
+    ): ResponseDto<Unit> {
+        return httpClient.post {
+            url {
+                appendPathSegments("api/v1/users/verify-email")
+            }
+            setBody(params)
+        }.body<ResponseDto<Unit>>()
+    }
+}
