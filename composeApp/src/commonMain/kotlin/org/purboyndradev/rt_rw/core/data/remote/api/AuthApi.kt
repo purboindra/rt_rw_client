b90ebc77dbeba06ebc1b8282c6dcc520a91a1207
purboindra <purboindra@gmail.com>
2025-11-22 05:12:46 +0700

    refactor: remove unused imports in AuthApi.kt

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/remote/api/AuthApi.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/remote/api/AuthApi.kt
@@ -4,8 +4,6 @@ import org.purboyndradev.rt_rw.core.data.dto.RefreshTokenDto
 import org.purboyndradev.rt_rw.core.data.dto.ResponseDto
 import org.purboyndradev.rt_rw.core.data.dto.SignInDto
 import org.purboyndradev.rt_rw.core.data.dto.VerifyOtpDto
-import org.purboyndradev.rt_rw.core.domain.AppError
-import org.purboyndradev.rt_rw.core.domain.Result
 
 interface AuthApi {
     suspend fun signIn(phoneNumber: String): ResponseDto<SignInDto>
