ffd871758a6918df0030e2f283aebfc024d962f3
purboindra <purboindra@gmail.com>
2025-12-05 04:56:40 +0700

    refactor: remove try-catch from RequestEmailVerificationUseCase

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/RequestEmailVerificationUseCase.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/RequestEmailVerificationUseCase.kt
@@ -3,16 +3,17 @@ package org.purboyndradev.rt_rw.domain.usecases
 import org.purboyndradev.rt_rw.core.data.remote.params.RequestEmailVerificationParams
 import org.purboyndradev.rt_rw.core.domain.AppError
 import org.purboyndradev.rt_rw.core.domain.Result
-import org.purboyndradev.rt_rw.core.network.mapKtorExceptionToAppError
 import org.purboyndradev.rt_rw.domain.repository.UserRepository
 
 class RequestEmailVerificationUseCase(private val userRepository: UserRepository) {
     suspend operator fun invoke(params: RequestEmailVerificationParams): Result<Unit, AppError> {
-        return try {
-            userRepository.requestEmailVerification(params)
-            Result.Success(Unit)
-        } catch (e: Exception) {
-            Result.Error(mapKtorExceptionToAppError(e))
+        return when(val result = userRepository.requestEmailVerification(params)){
+            is Result.Success -> {
+                Result.Success(Unit)
+            }
+            is Result.Error -> {
+                Result.Error(result.error)
+            }
         }
     }
 }
