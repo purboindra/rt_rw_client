952a4dd1b21df322f05d05fb634f9f38d5971681
purboindra <purboindra@gmail.com>
2025-12-05 04:56:05 +0700

    feat: wrap UserRepository methods with Result

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/repository/UserRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/repository/UserRepositoryImpl.kt
@@ -1,16 +1,35 @@
 package org.purboyndradev.rt_rw.core.data.repository
 
+import org.purboyndradev.rt_rw.core.data.datastore.AppAuthRepository
 import org.purboyndradev.rt_rw.core.data.remote.api.UserApi
 import org.purboyndradev.rt_rw.core.data.remote.params.RequestEmailVerificationParams
 import org.purboyndradev.rt_rw.core.data.remote.params.VerifyEmailParams
+import org.purboyndradev.rt_rw.core.domain.AppError
+import org.purboyndradev.rt_rw.core.domain.Result
+import org.purboyndradev.rt_rw.core.network.mapKtorExceptionToAppError
 import org.purboyndradev.rt_rw.domain.repository.UserRepository
 
-class UserRepositoryImpl(private val userApi: UserApi) : UserRepository {
-    override suspend fun requestEmailVerification(params: RequestEmailVerificationParams) {
-        userApi.requestEmailVerification(params)
+class UserRepositoryImpl(
+    private val userApi: UserApi,
+    private val appAuthRepository: AppAuthRepository
+) : UserRepository {
+    override suspend fun requestEmailVerification(params: RequestEmailVerificationParams): Result<Unit, AppError> {
+        return try {
+            userApi.requestEmailVerification(params)
+            Result.Success(Unit)
+        } catch (e: Exception) {
+            Result.Error(mapKtorExceptionToAppError(e))
+        }
     }
 
-    override suspend fun verifyEmail(params: VerifyEmailParams) {
-        userApi.verifyEmail(params)
+    override suspend fun verifyEmail(params: VerifyEmailParams): Result<Unit, AppError> {
+        return try {
+            userApi.verifyEmail(params)
+            appAuthRepository.saveEmail(params.email)
+            appAuthRepository.saveIsEmailVerified(true)
+            Result.Success(Unit)
+        } catch (e: Exception) {
+            Result.Error(mapKtorExceptionToAppError(e))
+        }
     }
 }
--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/repository/UserRepository.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/repository/UserRepository.kt
@@ -2,10 +2,12 @@ package org.purboyndradev.rt_rw.domain.repository
 
 import org.purboyndradev.rt_rw.core.data.remote.params.RequestEmailVerificationParams
 import org.purboyndradev.rt_rw.core.data.remote.params.VerifyEmailParams
+import org.purboyndradev.rt_rw.core.domain.AppError
+import org.purboyndradev.rt_rw.core.domain.Result
 
 interface UserRepository {
 
-    suspend fun requestEmailVerification(params: RequestEmailVerificationParams): Unit
-    suspend fun verifyEmail(params: VerifyEmailParams): Unit
+    suspend fun requestEmailVerification(params: RequestEmailVerificationParams): Result<Unit, AppError>
+    suspend fun verifyEmail(params: VerifyEmailParams): Result<Unit, AppError>
 
 }
