80cb4d68e1552310d3b2d66d05ec5a20fa558773
purboindra <purboindra@gmail.com>
2025-12-05 04:55:56 +0700

    feat: ReportState.kt

---
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/report/ReportState.kt
@@ -0,0 +1,10 @@
+package org.purboyndradev.rt_rw.features.report
+
+import org.purboyndradev.rt_rw.core.domain.model.ReportModel
+
+
+data class ReportState(
+    val isLoading: Boolean = false,
+    val reports: List<ReportModel> = emptyList(),
+    val error: String? = null
+)
