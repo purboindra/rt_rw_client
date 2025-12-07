c2710868fc05fe5e840f311172a09621400c2269
purboindra <purboindra@gmail.com>
2025-12-07 10:58:54 +0700

    refactor: remove try-catch from JoinActivityUseCase

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/JoinActivityUseCase.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/JoinActivityUseCase.kt
@@ -3,16 +3,18 @@ package org.purboyndradev.rt_rw.domain.usecases
 import org.purboyndradev.rt_rw.core.data.remote.params.JoinActivityParams
 import org.purboyndradev.rt_rw.core.domain.AppError
 import org.purboyndradev.rt_rw.core.domain.Result
-import org.purboyndradev.rt_rw.core.network.mapKtorExceptionToAppError
 import org.purboyndradev.rt_rw.domain.repository.ActivityRepository
 
 class JoinActivityUseCase(private val activityRepository: ActivityRepository) {
     suspend operator fun invoke(params: JoinActivityParams): Result<Unit, AppError> {
-        return try {
-            activityRepository.joinActivity(params)
-            Result.Success(Unit)
-        } catch (e: Exception) {
-            Result.Error(mapKtorExceptionToAppError(e))
+        return when (val result = activityRepository.joinActivity(params)) {
+            is Result.Success -> {
+                Result.Success(Unit)
+            }
+
+            is Result.Error -> {
+                Result.Error(result.error)
+            }
         }
     }
 }
