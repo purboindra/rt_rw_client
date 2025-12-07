203b7f8677e7493a9c16a5cacd0e83509ad90e0e
purboindra <purboindra@gmail.com>
2025-12-07 10:56:37 +0700

    feat: add activityId to ActivityDetailModel mapping

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/remote/mapper/ActivityDetailDtoToModel.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/remote/mapper/ActivityDetailDtoToModel.kt
@@ -2,19 +2,26 @@ package org.purboyndradev.rt_rw.core.data.remote.mapper
 
 import org.purboyndradev.rt_rw.core.data.dto.ActivityDetailDto
 import org.purboyndradev.rt_rw.core.domain.model.ActivityDetailModel
+import org.purboyndradev.rt_rw.helper.ActivityType
 
 fun ActivityDetailDto.toActivityDetailModel(): ActivityDetailModel {
     return ActivityDetailModel(
         id = id,
         title = title,
         description = description,
-        rt = rtId,
+        rt = rt,
         date = date,
         pic = pic,
         bannerImageUrl = bannerImageUrl,
         imageUrl = imageUrl,
-        type = type,
+        type = runCatching {
+            ActivityType.valueOf(type)
+        }.getOrElse {
+            println("Warning: Could not parse type '$type'. Defaulting to RONDA.")
+            ActivityType.RONDA
+        },
         createdBy = createdBy.toCreatedByModel(),
-        users = users
+        users = users,
+        activityId = activityId
     )
 }
