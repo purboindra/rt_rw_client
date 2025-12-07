7f7bdf9363dac654aa72058157be0d7db966e038
purboindra <purboindra@gmail.com>
2025-12-07 10:58:49 +0700

    refactor: remove try-catch from FetchUsersActivityUseCase

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/FetchUsersActivityUseCase.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/FetchUsersActivityUseCase.kt
@@ -3,16 +3,18 @@ package org.purboyndradev.rt_rw.domain.usecases
 import org.purboyndradev.rt_rw.core.domain.AppError
 import org.purboyndradev.rt_rw.core.domain.Result
 import org.purboyndradev.rt_rw.core.domain.model.UsersActivityModel
-import org.purboyndradev.rt_rw.core.network.mapKtorExceptionToAppError
 import org.purboyndradev.rt_rw.domain.repository.ActivityRepository
 
 class FetchUsersActivityUseCase(private val activityRepository: ActivityRepository) {
     suspend operator fun invoke(id: String): Result<List<UsersActivityModel>, AppError> {
-        return try {
-            val users = activityRepository.fetchUsersActivity(id)
-            Result.Success(users)
-        } catch (e: Exception) {
-            Result.Error(mapKtorExceptionToAppError(e))
+        return when (val result = activityRepository.fetchUsersActivity(id)) {
+            is Result.Success -> {
+                Result.Success(result.data)
+            }
+
+            is Result.Error -> {
+                Result.Error(result.error)
+            }
         }
     }
 }
