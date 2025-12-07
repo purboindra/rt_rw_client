9be9ec03764d2f9f342fa6a77c6c45401b3715a2
purboindra <purboindra@gmail.com>
2025-12-07 10:57:35 +0700

    feat: map rtId, activityId in ActivityDtoToModel

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/remote/mapper/ActivityDtoToModel.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/core/data/remote/mapper/ActivityDtoToModel.kt
@@ -2,21 +2,19 @@ package org.purboyndradev.rt_rw.core.data.remote.mapper
 
 import org.purboyndradev.rt_rw.core.data.dto.ActivityDto
 import org.purboyndradev.rt_rw.core.domain.model.ActivityModel
-import org.purboyndradev.rt_rw.core.domain.model.CreatedByModel
-import org.purboyndradev.rt_rw.core.domain.model.RTModel
-import org.purboyndradev.rt_rw.core.domain.model.UserModel
-import org.purboyndradev.rt_rw.domain.model.UserDBModel
 
 fun ActivityDto.toActivityModel(): ActivityModel {
     return ActivityModel(
         id = id,
         title = title,
         description = description,
-        rt = "",
+        rtId = rtId,
         date = date,
         type = type,
         pic = pic,
         bannerImageUrl = bannerImageUrl,
         imageUrl = imageUrl,
+        activityId=activityId,
+
     )
 }
