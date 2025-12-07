6758acf7fba1ed50667942c9d7252b83d62d3d28
purboindra <purboindra@gmail.com>
2025-12-07 10:58:38 +0700

    feat: pass params to FetchActivitiesUseCase

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/FetchActivitiesUseCase.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/FetchActivitiesUseCase.kt
@@ -1,18 +1,28 @@
 package org.purboyndradev.rt_rw.domain.usecases
 
+import org.purboyndradev.rt_rw.core.data.remote.params.PaginationParams
+import org.purboyndradev.rt_rw.core.data.remote.params.QueryParams
 import org.purboyndradev.rt_rw.core.domain.AppError
 import org.purboyndradev.rt_rw.core.domain.Result
 import org.purboyndradev.rt_rw.core.domain.model.ActivityModel
-import org.purboyndradev.rt_rw.core.network.mapKtorExceptionToAppError
 import org.purboyndradev.rt_rw.domain.repository.ActivityRepository
 
 class FetchActivitiesUseCase(private val activityRepository: ActivityRepository) {
-    suspend operator fun invoke(): Result<List<ActivityModel>, AppError> {
-        return try {
-            val activities = activityRepository.fetchAllActivities()
-            Result.Success(activities)
-        } catch (e: Exception) {
-            Result.Error(mapKtorExceptionToAppError(e))
+    suspend operator fun invoke(
+        queryParams: QueryParams? = null,
+        paginationParams: PaginationParams? = null
+    ): Result<List<ActivityModel>, AppError> {
+        return when (val result = activityRepository.fetchAllActivities(
+            queryParams = queryParams,
+            paginationParams = paginationParams
+        )) {
+            is Result.Success -> {
+                Result.Success(result.data)
+            }
+
+            is Result.Error -> {
+                Result.Error(result.error)
+            }
         }
     }
 }
