7b8ce68c543381abf6162a38810f4a5c51af6f16
purboindra <purboindra@gmail.com>
2025-12-07 12:20:44 +0700

    feat: add join button to ActivityHeaderSection

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/activity/presentation/ActivityHeaderSection.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/activity/presentation/ActivityHeaderSection.kt
@@ -1,12 +1,17 @@
 package org.purboyndradev.rt_rw.features.activity.presentation
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,33 +25,64 @@ import org.purboyndradev.rt_rw.helper.ActivityType
 import org.purboyndradev.rt_rw.helper.DateHelper
 
 @Composable
- fun ActivityHeaderSection(activity: ActivityDetailModel) {
-    Column {
-        Row(verticalAlignment = Alignment.CenterVertically) {
-            TypeChip(activity.type)
-            Spacer(Modifier.width(8.dp))
+fun ActivityHeaderSection(
+    activity: ActivityDetailModel, hasJoinActivity: Boolean = false,
+    onJoinActivity: () -> Unit,
+    isLoadingJoinActivity: Boolean,
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(modifier = Modifier.weight(1f)) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                TypeChip(activity.type)
+                Spacer(Modifier.width(8.dp))
+                Text(
+                    text = DateHelper.convertEpochToDate(activity.date),
+                    style = MaterialTheme.typography.labelMedium,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+
             Text(
-                text = DateHelper.convertEpochToDate(activity.date),
+                text = activity.title,
+                style = MaterialTheme.typography.headlineSmall,
+                fontWeight = FontWeight.SemiBold
+            )
+            Text(
+                text = activity.activityId ?: "",
                 style = MaterialTheme.typography.labelMedium,
-                color = MaterialTheme.colorScheme.onSurfaceVariant
+                color = MaterialTheme.colorScheme.primary
             )
         }
-
-        Spacer(Modifier.height(8.dp))
-
-        Text(
-            text = activity.title,
-            style = MaterialTheme.typography.headlineSmall,
-            fontWeight = FontWeight.SemiBold
-        )
-
-        Spacer(Modifier.height(4.dp))
-
-        Text(
-            text = activity.activityId ?: "",
-            style = MaterialTheme.typography.labelMedium,
-            color = MaterialTheme.colorScheme.primary
-        )
+        Spacer(modifier = Modifier.width(18.dp))
+        if (!hasJoinActivity) Box(modifier = Modifier.width(112.dp)) {
+            ElevatedButton(
+                onClick = onJoinActivity,
+                enabled = !isLoadingJoinActivity,
+                shape = RoundedCornerShape(8.dp),
+                border = BorderStroke(
+                    width = 1.dp,
+                    color = MaterialTheme.colorScheme.primaryContainer
+                ),
+                colors = ButtonColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    contentColor = MaterialTheme.colorScheme.primary,
+                    disabledContainerColor = MaterialTheme.colorScheme.primaryContainer.copy(
+                        alpha = 0.5f
+                    ),
+                    disabledContentColor = MaterialTheme.colorScheme.primary.copy(
+                        alpha = 0.5f
+                    )
+                ),
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    if (isLoadingJoinActivity) "Loading..." else "Join",
+                    style = MaterialTheme.typography.labelMedium
+                )
+            }
+        }
     }
 }
 
@@ -55,7 +91,7 @@ private fun TypeChip(type: ActivityType) {
     val label = when (type) {
         ActivityType.KEGIATAN_SOSIAL -> "Kegiatan Sosial"
         ActivityType.KERJA_BAKTI -> "Kerja Bakti"
-        ActivityType.RAPAT-> "Rapat"
+        ActivityType.RAPAT -> "Rapat"
         ActivityType.RONDA -> "Ronda"
     }
 
