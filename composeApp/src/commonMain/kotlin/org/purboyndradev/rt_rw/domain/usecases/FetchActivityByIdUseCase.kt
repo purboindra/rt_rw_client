37e1ce98de63ba12fcdbe21c54548d77e45a64c2
purboindra <purboindra@gmail.com>
2025-12-07 10:58:44 +0700

    feat: improve error handling in FetchActivityByIdUseCase

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/FetchActivityByIdUseCase.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/FetchActivityByIdUseCase.kt
@@ -3,16 +3,18 @@ package org.purboyndradev.rt_rw.domain.usecases
 import org.purboyndradev.rt_rw.core.domain.AppError
 import org.purboyndradev.rt_rw.core.domain.Result
 import org.purboyndradev.rt_rw.core.domain.model.ActivityDetailModel
-import org.purboyndradev.rt_rw.core.network.mapKtorExceptionToAppError
 import org.purboyndradev.rt_rw.domain.repository.ActivityRepository
 
 class FetchActivityByIdUseCase(private val activityRepository: ActivityRepository) {
     suspend operator fun invoke(id: String): Result<ActivityDetailModel, AppError> {
-        return try {
-            val activity = activityRepository.fetchActivityById(id)
-            Result.Success(activity)
-        } catch (e: Exception) {
-            Result.Error(mapKtorExceptionToAppError(e))
+        return when (val result = activityRepository.fetchActivityById(id)) {
+            is Result.Success -> {
+                Result.Success(result.data)
+            }
+
+            is Result.Error -> {
+                Result.Error(result.error)
+            }
         }
     }
 }
