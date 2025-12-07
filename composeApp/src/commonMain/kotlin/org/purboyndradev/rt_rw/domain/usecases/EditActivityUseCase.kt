ca09280ac78a7bf3c2d85117c797fd8420d0bad3
purboindra <purboindra@gmail.com>
2025-12-07 10:58:23 +0700

    refactor: EditActivityUseCase.kt

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/EditActivityUseCase.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/EditActivityUseCase.kt
@@ -3,7 +3,6 @@ package org.purboyndradev.rt_rw.domain.usecases
 import org.purboyndradev.rt_rw.core.data.remote.params.CreateActivityParams
 import org.purboyndradev.rt_rw.core.domain.AppError
 import org.purboyndradev.rt_rw.core.domain.Result
-import org.purboyndradev.rt_rw.core.network.mapKtorExceptionToAppError
 import org.purboyndradev.rt_rw.domain.repository.ActivityRepository
 
 class EditActivityUseCase(private val activityRepository: ActivityRepository) {
@@ -11,11 +10,14 @@ class EditActivityUseCase(private val activityRepository: ActivityRepository) {
         id: String,
         params: CreateActivityParams
     ): Result<Unit, AppError> {
-        return try {
-            activityRepository.editActivity(id, params)
-            Result.Success(Unit)
-        } catch (e: Exception) {
-            Result.Error(mapKtorExceptionToAppError(e))
+        return when (val result = activityRepository.editActivity(id, params)) {
+            is Result.Success -> {
+                Result.Success(Unit)
+            }
+
+            is Result.Error -> {
+                Result.Error(result.error)
+            }
         }
     }
 }
