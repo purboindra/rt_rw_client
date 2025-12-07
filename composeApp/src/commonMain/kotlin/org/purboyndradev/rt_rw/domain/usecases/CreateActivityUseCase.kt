7746900f1af01db4ffc7f4e88017b9cd4ae1ab03
purboindra <purboindra@gmail.com>
2025-12-07 10:58:05 +0700

    refactor: remove try-catch from CreateActivityUseCase

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/CreateActivityUseCase.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/domain/usecases/CreateActivityUseCase.kt
@@ -3,18 +3,20 @@ package org.purboyndradev.rt_rw.domain.usecases
 import org.purboyndradev.rt_rw.core.data.remote.params.CreateActivityParams
 import org.purboyndradev.rt_rw.core.domain.AppError
 import org.purboyndradev.rt_rw.core.domain.Result
-import org.purboyndradev.rt_rw.core.network.mapKtorExceptionToAppError
 import org.purboyndradev.rt_rw.domain.repository.ActivityRepository
 
 class CreateActivityUseCase(private val activityRepository: ActivityRepository) {
     suspend operator fun invoke(
         params: CreateActivityParams
     ): Result<Unit, AppError> {
-        return try {
-            activityRepository.createActivity(params)
-            Result.Success(Unit)
-        } catch (e: Exception) {
-            Result.Error(mapKtorExceptionToAppError(e))
+        return when (val result = activityRepository.createActivity(params)) {
+            is Result.Success -> {
+                Result.Success(Unit)
+            }
+
+            is Result.Error -> {
+                Result.Error(result.error)
+            }
         }
     }
 }
