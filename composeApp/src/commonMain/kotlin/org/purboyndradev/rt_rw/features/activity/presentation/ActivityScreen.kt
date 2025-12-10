2a3f9b439dd7f4c202a5b20bfa6ff8b67b6c82ea
purboindra <purboindra@gmail.com>
2025-12-11 05:18:29 +0700

    feat: add search functionality to ActivityScreen

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/activity/presentation/ActivityScreen.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/activity/presentation/ActivityScreen.kt
@@ -8,22 +8,27 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import compose.icons.FeatherIcons
 import compose.icons.feathericons.Plus
 import org.koin.compose.viewmodel.koinViewModel
-import org.koin.core.parameter.parametersOf
+import org.purboyndradev.rt_rw.core.data.remote.params.QueryParams
 import org.purboyndradev.rt_rw.core.domain.model.ActivityModel
 import org.purboyndradev.rt_rw.features.components.EmptyStateView
 import org.purboyndradev.rt_rw.features.components.ErrorViewCompose
@@ -35,10 +40,12 @@ fun ActivityScreen(
     modifier: Modifier = Modifier,
     navHostController: NavHostController
 ) {
-    val activityViewModel = koinViewModel<ActivityViewModel>(parameters = {
-        parametersOf("1")
-    })
+    val activityViewModel = koinViewModel<ActivityViewModel>()
     val activityState by activityViewModel.activitiesState.collectAsStateWithLifecycle()
+
+    val searchQuery by activityViewModel.searchQuery.collectAsStateWithLifecycle()
+
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     LaunchedEffect(Unit) {
         activityViewModel.fetchActivities()
@@ -61,6 +68,38 @@ fun ActivityScreen(
                 )
             }
         }
+
+
+        OutlinedTextField(
+            value = searchQuery,
+            onValueChange = {
+                activityViewModel.onQueryChange(it)
+            },
+            modifier = modifier.fillMaxWidth().padding(
+                horizontal = 18.dp
+            ),
+            placeholder = {
+                Text("Search")
+            },
+            singleLine = true,
+            maxLines = 1,
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+
+                    val queryParams = QueryParams(
+                        query = searchQuery
+                    )
+
+                    activityViewModel.fetchActivities(queryParams)
+
+                    keyboardController?.hide()
+                }
+            )
+        )
+
         when {
             activityState.loading -> {
                 LoadingIndicatorCompose(modifier)
