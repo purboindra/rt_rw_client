229baffdfaffa495afbf60355c6a6c9b0e2c8213
purboindra <purboindra@gmail.com>
2025-12-11 05:24:33 +0700

    feat: add search functionality to ReportsScreen

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/report/presentation/ReportsScreen.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/report/presentation/ReportsScreen.kt
@@ -9,10 +9,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -20,12 +23,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import compose.icons.FeatherIcons
 import compose.icons.feathericons.Plus
 import org.koin.compose.viewmodel.koinViewModel
+import org.purboyndradev.rt_rw.core.data.remote.params.QueryParams
 import org.purboyndradev.rt_rw.features.components.EmptyStateView
 import org.purboyndradev.rt_rw.features.components.ErrorViewCompose
 import org.purboyndradev.rt_rw.features.components.ReportContent
@@ -40,6 +46,9 @@ fun ReportsScreen(
     val reportViewModel = koinViewModel<ReportViewModel>()
 
     val reportsState by reportViewModel.reportsState.collectAsStateWithLifecycle()
+    val searchQuery by reportViewModel.searchQuery.collectAsStateWithLifecycle()
+
+    val keyboardController = LocalSoftwareKeyboardController.current
 
     LaunchedEffect(Unit) {
         reportViewModel.fetchAllReports()
@@ -64,6 +73,36 @@ fun ReportsScreen(
                 )
             }
         }
+
+        OutlinedTextField(
+            value = searchQuery,
+            onValueChange = {
+                reportViewModel.onQueryChange(it)
+            },
+            modifier = modifier.fillMaxWidth().padding(
+                horizontal = 18.dp
+            ),
+            placeholder = {
+                Text("Search")
+            },
+            singleLine = true,
+            maxLines = 1,
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+
+                    val queryParams = QueryParams(
+                        query = searchQuery
+                    )
+
+                    reportViewModel.fetchAllReports(queryParams)
+
+                    keyboardController?.hide()
+                }
+            )
+        )
 
         when {
             reportsState.isLoading -> {
