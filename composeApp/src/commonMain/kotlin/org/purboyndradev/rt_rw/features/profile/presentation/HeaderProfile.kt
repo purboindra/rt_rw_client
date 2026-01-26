aba43f40fd0a39786291e2fcd4e5ac6d57831d90
purboindra <purboindra@gmail.com>
2026-01-26 22:10:43 +0700

    feat: profile picture HeaderProfile.kt

--- composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/profile/presentation/HeaderProfile.kt
+++ composeApp/src/commonMain/kotlin/org/purboyndradev/rt_rw/features/profile/presentation/HeaderProfile.kt
@@ -5,19 +5,26 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import compose.icons.FeatherIcons
+import compose.icons.feathericons.User
 
 @Composable
 fun HeaderProfile(
@@ -32,9 +39,20 @@ fun HeaderProfile(
             modifier = modifier.size(64.dp).clip(
                 shape = RoundedCornerShape(100)
             ).background(
-                color = Color.Gray
+                color = Color.LightGray.copy(alpha = 0.35f)
+            ),
+            contentAlignment = Alignment.Center
+        ) {
+            if (imageUrl == null) Icon(
+                FeatherIcons.User, contentDescription = "Empty Profile Picture",
+                tint = Color.Black
+            ) else AsyncImage(
+                model = imageUrl,
+                contentScale = ContentScale.Crop,
+                contentDescription = "Profile Picture",
+                modifier = Modifier.fillMaxWidth(),
             )
-        )
+        }
         Spacer(modifier = modifier.width(8.dp))
         Column {
             Text(
